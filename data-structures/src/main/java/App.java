78729a65ed23a1aa9bc96116627df7060565aa23
kulkarnianiket <kulkarnianiket929@gmail.com>
2015-08-23 21:53:17 -0500

    testing auto password and username again

--- data-structures/src/main/java/App.java
+++ data-structures/src/main/java/App.java
@@ -6,7 +6,7 @@ package java;
  */
 public class App 
 {
-    //main function
+    //main function.
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
